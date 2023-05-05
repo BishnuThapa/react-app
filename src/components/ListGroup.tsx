2d0c02205f2b1455fb90d60efdca03ac80ba1505
Bishnu <thapabishnu20@gmail.com>
2023-05-05 15:23:39 +0545

    handling events

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,8 +1,12 @@
+import { MouseEvent } from "react";
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   //   items = [];
   //   if (items.length === 0) return <p>No Items Found.</p>;
   // map function is used to converting each item to an different type
+
+  //  event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
   return (
     //   Fragments for component can return multiple lines <> </>
     <>
@@ -11,8 +15,8 @@ function ListGroup() {
       {items.length === 0 && <p>No Items Found</p>}
       <ul className="list-group">
         {/*  to return data dynamically write inside {} */}
-        {items.map((item) => (
-          <li key={item} className="list-group-item">
+        {items.map((item, index) => (
+          <li key={item} className="list-group-item" onClick={handleClick}>
             {item}
           </li>
         ))}
