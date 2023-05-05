fe4ae1743ce25c6a30e706551db98cc193340c4c
Bishnu <thapabishnu20@gmail.com>
2023-05-05 15:11:28 +0545

    conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,14 @@
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  //   items = [];
+  //   if (items.length === 0) return <p>No Items Found.</p>;
   // map function is used to converting each item to an different type
   return (
     //   Fragments for component can return multiple lines <> </>
     <>
       <h1>List</h1>
+      {/* {items.length === 0 ? <p>No Items Found</p> : null} */}
+      {items.length === 0 && <p>No Items Found</p>}
       <ul className="list-group">
         {/*  to return data dynamically write inside {} */}
         {items.map((item) => (
